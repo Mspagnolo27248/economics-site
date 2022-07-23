4cb96af873e0b107ca35203c46bd3c9630fce53e
Michael Spagnolo <mspagnolo27@gmail.com>
2022-07-22 22:23:46 -0400

    Complete fetch() backend and Chart JS

--- pages/api/cpi.js
+++ pages/api/cpi.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-    fetch('https://api.stlouisfed.org/fred/series/observations?series_id=MORTGAGE30US&observation_start=2022-07-01&observation_end=2022-07-22&api_key=3930e57c78d990d30f7d376f5c406e2e&file_type=json')
+    fetch('https://api.stlouisfed.org/fred/series/observations?series_id=MORTGAGE30US&observation_start=2019-07-01&observation_end=2022-07-22&api_key=3930e57c78d990d30f7d376f5c406e2e&file_type=json')
     .then((response)=>response.json())
     .then((data)=> res.send(data))
    
--- pages/data/Cpi.js
+++ pages/data/Cpi.js
@@ -1,9 +1,53 @@
 import React, { Fragment, useEffect,useState } from 'react'
 import Table from '../../components/ui/table'
-export default function CpiPage() {
+import { Line } from 'react-chartjs-2';
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' ,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+export default function CpiPage() {
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  )
 const [dataCpi,setData] = useState([{date:"mike",value:10}]);
 
+const labels = dataCpi.map((item)=>item.date);
+const chartData = {
+  labels: labels,
+  datasets: [{
+    label: 'My First dataset',
+    borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    data: dataCpi.map((item)=>item.value),
+  }]
+}
 useEffect(()=>{
 //https://api.stlouisfed.org/fred/series/observations?series_id=MORTGAGE30US&realtime_start=2022-01-22&realtime_end=2022-07-22&api_key=3930e57c78d990d30f7d376f5c406e2e&file_type=json
 fetch('/api/cpi')
@@ -21,8 +65,21 @@ fetch('/api/cpi')
 
   return (
     <Fragment>
+      <div className='chart-container'>
+      <Line  data={chartData}  options={options} />
+      </div>
 
-        <Table data = {dataCpi}/>
+      <div className='chart-container'>
+      <Line  data={chartData}  options={options} />
+      </div>
+
+      <div className=''>
+      <Table data = {dataCpi}/>
+      </div>
+      <div className=''>
+      <Table data = {dataCpi}/>
+      </div>
+     
     </Fragment>
   )
 }
