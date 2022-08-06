d8322a3362656beab1545e0ace07890e7118c1dd
Michael Spagnolo <mspagnolo27@gmail.com>
2022-08-06 14:10:34 -0400

    release/dates complete

--- pages/releases/dates.js
+++ pages/releases/dates.js
@@ -18,42 +18,42 @@ export default function ReleaseDates(props) {
 
 
 export async function getStaticProps(){
-  const fredCodes = GetTestData('FRED-series.json');
-  const seriesList = await FetchReleaseCodes(Object.values(fredCodes))
+  const seriesList = GetTestData('ReleaseCodes.json');
+  const  FutureReleaseDate = []
 
-const getFredSync = async (series_ids)=>{
-    const  FutureReleaseDate = []
-    const releaseData = []
-    for(const series in series_ids){
-      const serName = series_ids[series].releases[0].name
-      const releaseid = series_ids[series].releases[0].id
-      releaseData.push({seriesName:serName,seriesId:series,releaseId:releaseid, })
-      const fredUrl = `https://api.stlouisfed.org/fred/release/dates?release_id=${releaseid}&`+
+const getFredSync = async (release_id)=>{   
+      const fredUrl = `https://api.stlouisfed.org/fred/release/dates?release_id=${release_id}&`+
       "include_release_dates_with_no_data=true&sort_order=desc&limit"+
       "=4&api_key=3930e57c78d990d30f7d376f5c406e2e&file_type=json"
       const data = await fetch(fredUrl);
       const jsonData = await data.json();
-      const obj = {}
-      obj[releaseid]=  await jsonData.release_dates.map((item)=>item.date)
-      FutureReleaseDate.push(obj)
-      }
-      console.log(JSON.stringify(releaseData))
-      return releaseData
+      return jsonData
       }
 
+      for (const series of seriesList){
+        const releaseDates = await getFredSync(series.releaseId);
+        const obj = {}
+        obj[series.seriesId]= releaseDates.release_dates.map((item)=>item.date)
+        FutureReleaseDate.push(obj)
+       
+   
+      }
+   
 
-const data =  getFredSync(seriesList)
-.then((output)=> {return{
-    props:{
-        data:output
-    }
-}});
 
-return data
+      return  {props:{
+        data:FutureReleaseDate
+      }
+      }
+      }
+
+
+
+
+
    
 
 
 
    
-}
-   
+
