4681b2f98f129ad70768c4be1d1282db4e2fb6ae
Michael Spagnolo <mspagnolo27@gmail.com>
2022-08-06 13:37:37 -0400

    releases/dates works w debug not prod

--- pages/releases/dates.js
+++ pages/releases/dates.js
@@ -1,6 +1,6 @@
 import React from 'react'
+import { GetTestData } from '../../Utilities/fs_wrapper';
 import { FetchReleaseCodes } from '../../Utilities/fetch-fred'
-import { GetTestData } from '../../Utilities/fs_wrapper'
 
 export default function ReleaseDates(props) {
 
@@ -19,29 +19,27 @@ export default function ReleaseDates(props) {
 
 export async function getStaticProps(){
   const fredCodes = GetTestData('FRED-series.json');
-const seriesList = await FetchReleaseCodes(Object.values(fredCodes))
+  const seriesList = await FetchReleaseCodes(Object.values(fredCodes))
 
 const getFredSync = async (series_ids)=>{
     const  FutureReleaseDate = []
-
+    const releaseData = []
     for(const series in series_ids){
+      const serName = series_ids[series].releases[0].name
       const releaseid = series_ids[series].releases[0].id
+      releaseData.push({seriesName:serName,seriesId:series,releaseId:releaseid, })
       const fredUrl = `https://api.stlouisfed.org/fred/release/dates?release_id=${releaseid}&`+
       "include_release_dates_with_no_data=true&sort_order=desc&limit"+
       "=4&api_key=3930e57c78d990d30f7d376f5c406e2e&file_type=json"
       const data = await fetch(fredUrl);
-      const json = await data.json();
-      if(json){
-        const obj = {}
-        obj[releaseid]= await json.release_dates.map((item)=>item.date)
-        FutureReleaseDate.push(obj)
+      const jsonData = await data.json();
+      const obj = {}
+      obj[releaseid]=  await jsonData.release_dates.map((item)=>item.date)
+      FutureReleaseDate.push(obj)
       }
-    
-    }
-
-    console.log(FutureReleaseDate)
-    return FutureReleaseDate
-}
+      console.log(JSON.stringify(releaseData))
+      return releaseData
+      }
 
 
 const data =  getFredSync(seriesList)
